42f8d4cbdb3e43248a90011663e5c6b3da661610
ishitaanand2222 <ishitaanand2222@gmail.com>
2023-05-06 19:39:26 +0530

    Manipulating the state

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
-import React from 'react';
+import React,{Component} from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-function App() {
-  return (
+class App extends Component  {
+  state = {
+    persons : [
+      {name: "Ishita", age: 28},
+      {name: "Anand", age:23},
+      {name: "Savvy", age:45}
+    ]
+  };
+  
+  switchNameHandler = () =>{
+    this.setState({
+      persons : [
+        {name: "Ishita Anand", age: 28},
+        {name: "Ishu", age:23},
+        {name: "Savvy", age:47}
+      ]
+    })
+  }
+  render() {
     //we can have only root element
     //JSX
-    <div className="App">
-      <h1>Hi, I am a react App</h1>
-      <p>This is really working</p>
-      <Person name = "ishita" age="22"/>
-      <Person name = "anand" age = "23"/>
-      <Person name = "savvy" age = "45">My hobby: cooking</Person>
-    </div>
-  );
+    return(
+      <div className="App">
+        <h1>Hi, I am a react App</h1>
+        <p>This is really working</p>
+        <button onClick = {this.switchNameHandler} >Switch Name</button>
+        <Person name = {this.state.persons[0].name} age= {this.state.persons[0].age} />
+        <Person name = {this.state.persons[1].name} age = {this.state.persons[1].age} />
+        <Person name = {this.state.persons[2].name} age = {this.state.persons[2].age}>My hobby: cooking</Person>
+      </div>
+    );
+  };
+
 
   //This is internal compilation of above JSX
   // return(
